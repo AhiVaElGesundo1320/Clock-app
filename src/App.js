577db2f233847848fa8880f07217a24c080d9767
Ethan <ethangmg@gmail.com>
2024-02-21 14:07:20 -0600

    creando darkMode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 
 import { Main } from "./Components/Main";
+import { ThemeContext } from "./Components/ThemeContext/ThemeContext";
 
 function App() {
   return (
-    <div className="App h-screen flex flex-col justify-center bg-gradient-to-r from-cyan-950 to-teal-700 text-white">
+    <div className="App h-screen flex flex-col justify-center bg-gradient-to-r from-teal-300 to-cyan-500 dark:bg-gradient-to-r dark:from-cyan-950 dark:to-teal-700 text-white">
+      <ThemeContext>
       <Main />
+      </ThemeContext>
     </div>
   );
 }
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,13 +3,16 @@ import React from "react";
 //import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; //
 import { Navigation } from "./Navigation";
-
-
+import { useTheme } from "./ThemeContext/ThemeContext";
 
 export const Main = () => {
+  const themeActive = useTheme();
   return (
     <div>
       <Navigation />
+      <button onClick={themeActive}>
+        <span class="material-symbols-outlined">contrast</span>
+      </button>
     </div>
   );
 };
